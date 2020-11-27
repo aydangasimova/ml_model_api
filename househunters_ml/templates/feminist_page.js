761e1fd00940b3a658caf596d7f4820cbeb47194
Gasimova, Aydan <aydan.gasimova@kpn.com>
2020-11-27 10:31:30 +0100

    adds functionality of test results

--- househunters_ml/templates/feminist_page.js
+++ househunters_ml/templates/feminist_page.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 var $checks = $('input:checkbox').click(function(e) {
     var numChecked = $checks.filter(':checked').length;
-    if (numChecked > 3) {
-        alert("sorry, you have already selected 3 checkboxes!");
-    }});
+    if (numChecked >= 3) {
+        $("#test-result").text("lalala");
+    } else {$("#test-result").text("Blalala");}
+});
 }
 )
